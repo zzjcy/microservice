831a55b4286a80f2684e4e8d1b8747d128b25203
zhangzhi <zhangzhi@zhongbaofu.com>
2018-06-12 15:04:47 +0800

    test commit

---
+++ configclient/src/main/java/com/fubaofei/config/client/controller/ConfigController.java
@@ -0,0 +1,23 @@
+package com.fubaofei.config.client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Administrator
+ * @description
+ * @date 2018/6/12
+ */
+@RestController
+@RequestMapping("/cfg")
+public class ConfigController {
+
+    @Value("${foo}")
+    String foo;
+
+    @RequestMapping(value = "/foo")
+    public String foo(){
+        return foo + "——";
+    }
+}
